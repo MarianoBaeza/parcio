7094ad141f219200f33863928180bee1ecec9cac
Mariano Baeza <marianobaeza@hotmail.com>
2021-07-23 13:00:04 -0300

    fixes nav view

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import s from './Home.module.scss';
+import {BsSearch} from 'react-icons/bs';
+
+export const Home = () => {
+    return (
+        <div>
+            <div class={`d-flex flex-column vh-100`}>
+                <div class={`d-flex justify-content-center flex-column flex-grow-1 align-items-center ${s['main-bg']}`} style={{backgroundImage: "url('./img/avenue.jpg')"}} >
+                    <h1 class={`text-light ${s.title}`}>Welcome to Parcio</h1>
+                    <div class={s.searchbar}>
+                        <input type='text' placeholder='Search country' class={s['search-input']} />
+                        <button class={s['search-button']}>
+                            <BsSearch color='white' />
+                        </button>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
