1f6025bf294b29bedd8ea6d0fc2af8ef2b67bef0
Mariano Baeza <marianobaeza@hotmail.com>
2021-07-23 14:36:49 -0300

    adds main component

---
+++ src/components/Main/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import List from './ParkingsList';
+
+export const Main = () => {
+    return (
+        <div>
+            <List />
+        </div>
+    )
+};
+
+export default Main;
