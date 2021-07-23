c753ed2735353634a870c9c47be3d0b464cb24fd
Mariano Baeza <marianobaeza@hotmail.com>
2021-07-23 12:52:21 -0300

    adds routes in app and sass

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
-import Home from './components/Home'
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Nav from './components/Nav';
+import Main from './components/Main';
 
 function App() {
   return (
-    <Home />
+    <BrowserRouter>
+      <Route path='/' component={Nav} />
+      <Route exact path='/' component={Home} />
+      <Route exact path='/main' component={Main} />
+    </BrowserRouter>
   );
 }
 
